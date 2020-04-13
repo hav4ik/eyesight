39cd452cc85c7d39bf0ccefbb79314a1b9dcd26f
hav4ik <hav4ik@users.noreply.github.com>
2020-04-12 21:07:06 -0700

    Added sync adapter

--- eyesight/engine/adapters.py
+++ eyesight/engine/adapters.py
@@ -1,10 +1,16 @@
 from abc import ABCMeta, abstractmethod
 from copy import deepcopy
+from collections import deque
 import asyncio
+import time
 
 
 class BaseInputAdapter(metaclass=ABCMeta):
     """Base class for input adapters.
+
+    # Arguments:
+        input_services: a `dict` {'service_name': service}, where service
+            inherits from BaseService
     """
     def __init__(self, input_services=dict()):
         if isinstance(input_services, list):
@@ -24,6 +30,10 @@ class BaseInputAdapter(metaclass=ABCMeta):
 
 class SimpleAdapter(BaseInputAdapter):
     """Simply get what it is asked
+
+    # Arguments:
+        input_services: a `dict` {'service_name': service}, where service
+            inherits from BaseService
     """
     def __init__(self, input_services):
         super().__init__(input_services)
@@ -37,12 +47,18 @@ class LatestAdapter(BaseInputAdapter):
     """
     Stores the latest frame of each service and returns when updates
     are available
+
+    # Arguments:
+        input_services: a `dict` {'service_name': service}, where service
+            inherits from BaseService
     """
     def __init__(self, input_services):
         super().__init__(input_services)
-        self._saved_outputs = dict((k, None) for k in input_services)
-        self._coroutines = [self.get(input_id)
-                            for input_id in input_services]
+        self._saved_outputs = dict((k, None) for k in input_services.keys())
+        self._coroutines = [
+                self.get(input_id) for input_id in input_services.keys()]
+        self._coroutine_ids = dict(
+                zip(input_services.keys(), range(len(input_services))))
         self._lock = asyncio.Lock()
 
     @asyncio.coroutine
@@ -50,16 +66,65 @@ class LatestAdapter(BaseInputAdapter):
         while True:
             with self._lock:
                 self._saved_outputs[input_id] = \
-                    self._input_services[input_id].query().copy()
+                    self._input_services[input_id].query()
             yield self._saved_outputs[input_id]
 
     async def get_inputs(self, *input_ids):
-        await asyncio.wait(self._coroutines,
+        await asyncio.wait(self._coroutines[self._coroutine_ids[input_ids]],
                            return_when=asyncio.FIRST_COMPLETED)
         with self._lock:
             # TODO: too many copies!
             return dict((input_id, deepcopy(self._saved_outputs))
                         for input_id in input_ids)
+
+
+class SyncAdapter(BaseInputAdapter):
+    """Returns only the frames with closest timestamps.
+
+    # Arguments:
+        input_services: a `dict` {'service_name': service}, where service
+            inherits from BaseService
+        max_cache: number of frames to store in cache. If set to None,
+            the cache is unlimited (beware memory issues!)
+    """
+    def __init__(self, input_services, max_cache_size=200):
+        super().__init__(input_services)
+        self._cache = dict()
+        for service_name, service in input_services.items():
+            self._cache[service_name] = deque(maxlen=max_cache_size)
+        self._coroutines = [
+                self.get(input_id) for input_id in input_services]
+        self._coroutine_ids = dict(
+                zip(input_services.keys(), range(len(input_services))))
+        self._lock = asyncio.Lock()
+
+    @asyncio.coroutine
+    def get(self, input_id):
+        while True:
+            with self._lock:
+                last = self._input_services[input_id].query()
+                self._cache[input_id].append(last)
+            yield last
+
+    async def get_inputs(self, *input_ids):
+        await asyncio.wait(self._coroutines[self._coroutine_ids[input_ids]],
+                           return_when=asyncio.FIRST_COMPLETED)
+        ret = dict()
+        with self._lock:
+            earliest = time.time()
+            for input_id in input_ids:
+                # deque -> last element -> history -> last entry
+                t = self._cache[input_id][-1][0][-1].timestamp
+                earliest = min(t, earliest)
+
+            for input_id in input_ids:
+                # deque -> first element -> history -> last entry
+                while self._cache[input_id][0][0][-1].timestamp < earliest:
+                    ret[input_id] = self._cache[input_id].popleft()
+
+        # We can do that outside the lock because the `ret` contains only
+        # elements that got popped out already.
+        return ret
 
 
 class MultiAdapter(BaseInputAdapter):
@@ -84,6 +149,8 @@ class MultiAdapter(BaseInputAdapter):
 
 
 def get(identifier):
+    """An easy interface for one to load any adapters available.
+    """
     if isinstance(identifier, str):
         if identifier == 'simple':
             return SimpleAdapter
